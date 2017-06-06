03337c6cc60636fb27528563ff14f4ccc8771d2f
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2017-06-06 19:57:23 +0200

    final release for this mvp /BB

--- src/HaxBot_with_DistanceSensor/HaxBot_with_DistanceSensor.cpp
+++ src/HaxBot_with_DistanceSensor/HaxBot_with_DistanceSensor.cpp
@@ -5,6 +5,8 @@
 
 
 // #define DEBUG 1
+#define STEPS 4076 / 4
+
 #define FORWARD 0
 #define LEFT 1
 #define RIGHT 2
@@ -13,61 +15,133 @@
 
 DistanceSensor *ds;
 Stepper *stepper;
-uint16_t maxRange = 200;
 uint16_t loopCount = 0;
 uint16_t loopMaxCount = 10000;
-uint8_t turn = 0;
+uint16_t stepOfMove = 0;
+uint8_t turn = FORWARD;
 
 
 
 
 void setup() {
 	Serial.begin(115200);
-
 	Serial.println("### START PROGRAM ###");
 
 	stepper = new Stepper;
 	ds = new DistanceSensor;
 
+	// Timer0 is already used for millis() - we'll just interrupt somewhere
+	// in the middle and call the "Compare A" function below
+//	OCR0A = 0xAF;
+//	TIMSK0 |= _BV(OCIE0A);
+
 	delay(1000);
 
+}
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+//SIGNAL(TIMER0_COMPA_vect) {
+//	Serial.println("timer");
+//} 
+
+
+
+void turnLeft(int distance) {
+	if (stepOfMove == 0) {
+		stepper->turnLeft(distance);
+		stepOfMove = 1;
+	} else {
+		uint16_t distance = stepper->getDistanceToGo();
+		if (distance == 0) {
+			stepOfMove = 0;
+		} else {
+			stepper->executeMove();
+		}
+	}
+}
+
+void turnRight(int distance) {
+	if (stepOfMove == 0) {
+		stepper->turnRight(distance);
+		stepOfMove = 1;
+	} else {
+		uint16_t distance = stepper->getDistanceToGo();
+		if (distance == 0) {
+			stepOfMove = 0;
+		} else {
+			stepper->executeMove();
+		}
+	}
+
+}
+
+void moveForward(int distance) {
+	if (stepOfMove == 0) {
+		stepper->moveForward(distance);
+		stepOfMove = 1;
+	} else {
+		uint16_t distance = stepper->getDistanceToGo();
+		if (distance == 0) {
+			stepOfMove = 0;
+		} else {
+			stepper->executeMove();
+		}
+	}
+}
+
+void moveBackward(int distance) {
+	if (stepOfMove == 0) {
+		stepper->moveBackward(distance);
+		stepOfMove = 1;
+	} else {
+		uint16_t distance = stepper->getDistanceToGo();
+		if (distance == 0) {
+			stepOfMove = 0;
+		} else {
+			stepper->executeMove();
+		}
+	}
 }
 
 
 void loop() {
 
-	// move robot
-	if (loopCount % 100 == 0) {
-		if (turn == LEFT) {
-			stepper->turnLeft(1000);
-		} else if (turn == RIGHT) {
-			stepper->turnRight(1000);
+
+	// detect obstacles
+	if (loopCount % 10001 == 0) {
+		long distance = ds->getDistance();
+	}
+
+	// detect obstacles
+	if (loopCount % 10001 == 0) {
+		long distance = ds->getDistance();
+		Serial.println(distance);
+		if (distance == 0) {
+		} else if (distance < 20) {
+			if (turn == FORWARD) {
+				turn = LEFT;
+				stepOfMove = 0;
+			} else {
+				turn = FORWARD;
+			}
 		} else {
-			stepper->moveForward(1000);
+			turn = FORWARD;
 		}
+		loopCount = 0;
 	}
 
 
-	// detect obstacles
-	if (loopCount >= loopMaxCount) {
-		long distance = ds->getDistance();
-#ifdef DEBUG
-		Serial.print(distance);
-		Serial.println(" cm");
-#endif
-		if (distance == 0) {
-		} else if (distance < 20) {
-			turn = LEFT;
-#ifdef DEBUG
-			Serial.println("# turn left");
-#endif
-		} else {
-			turn = FORWARD;
-#ifdef DEBUG
-			Serial.println("# move forward");
-#endif
+	// move robot
+	if (loopCount % 100 == 0) {
+		if (turn == LEFT) {
+			turnLeft(STEPS);
+		} else if (turn == RIGHT) {
+			turnRight(STEPS);
+		} else if (turn == FORWARD) {
+			moveForward(STEPS);
+		} else if (turn == BACKWARD) {
+			moveBackward(STEPS);
 		}
-		loopCount = 0;
 	}
 
 
